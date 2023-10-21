9051c57abdc18bb7baf3d24153077608e7d42e17
Alon Landmann <rafael.landmann@gmail.com>
2023-10-21 09:45:32 +0800

    add ui tooltips

--- components/ranges/UiButtons.js
+++ components/ranges/UiButtons.js
@@ -1,11 +1,18 @@
+import Tooltip from '@mui/material/Tooltip'
 import css from '@/scss/ranges/UiButtons.module.scss'
 
 export default function UiButtons({ saveChanges }) {
   return (
     <div className={css.container}>
-      <div onClick={saveChanges}><i className='bi bi-floppy'></i></div>
-      <div><i className='bi bi-pen'></i></div>
-      <div><i className='bi bi-trash3'></i></div>
+      <Tooltip arrow title='save changes' placement='top' enterDelay={500}>
+        <div onClick={saveChanges}><i className='bi bi-floppy'></i></div>
+      </Tooltip>
+      <Tooltip arrow title='edit range' placement='top' enterDelay={500}>
+        <div><i className='bi bi-pen'></i></div>
+      </Tooltip>
+      <Tooltip arrow title='delete range' placement='top' enterDelay={500}>
+        <div><i className='bi bi-trash3'></i></div>
+      </Tooltip>
     </div>
   )
 }
