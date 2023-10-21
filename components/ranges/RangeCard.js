922cce3865cfa1d24c74b35b1b2773c78ae305b1
Alon Landmann <rafael.landmann@gmail.com>
2023-10-21 08:46:41 +0800

    add legend

--- components/ranges/RangeCard.js
+++ components/ranges/RangeCard.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { cloneDeep } from 'lodash'
+import { v4 as uuid } from 'uuid'
 import Matrix from '@/components/common/Matrix'
+import { fontColors } from '@/lib/colors'
 import css from '@/scss/ranges/RangeCard.module.scss'
 
 export default function RangeCard({ range }) {
@@ -8,8 +10,26 @@ export default function RangeCard({ range }) {
 
   return (
     <div>
-      <div className={css.name}>{formData.name}</div>
-      <Matrix range={formData} setFormData={setFormData} />
+      <div className={css.name}>
+        {formData.name}
+      </div>
+      <div className={css.matrix}>
+        <Matrix range={formData} setFormData={setFormData} />
+      </div>
+      <div className={css.legend}>
+        {range.options.slice(1).reverse().map(option => (
+          <div
+            key={uuid()}
+            style={{
+              border: `1px solid ${fontColors[option.color]}33`,
+              color: fontColors[option.color],
+              backgroundColor: option.color
+            }}
+          >
+            {option.description}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
