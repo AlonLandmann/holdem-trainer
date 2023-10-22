98dfb355ffb2d99323ea884d7f469a5b03e90393
Alon Landmann <rafael.landmann@gmail.com>
2023-10-23 00:18:07 +0800

    nicer add button

--- components/ranges/UiSection.js
+++ components/ranges/UiSection.js
@@ -1,5 +1,4 @@
 import { cloneDeep } from 'lodash'
-import Button from '@/components/common/Button'
 import { putUser } from '@/db/dbFetch'
 import { newRange } from '@/lib/ranges'
 import css from '@/scss/ranges/UiSection.module.scss'
@@ -15,14 +14,10 @@ export default function UiSection({ user }) {
 
   return (
     <div className={css.container}>
-      <Button
-        large
-        theme='simple'
-        icon='plus-lg'
-        onClick={addSpot}
-      >
-        new range
-      </Button>
+      <div className={css.add} onClick={addSpot}>
+        <i className='bi bi-plus-lg'></i>
+        <div>add new range</div>
+      </div>
     </div>
   )
 }
