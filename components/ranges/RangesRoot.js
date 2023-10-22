9c6070334159a735b52eb844fbf68cda36347544
Alon Landmann <rafael.landmann@gmail.com>
2023-10-22 14:53:23 +0800

    add new range

--- components/ranges/RangesRoot.js
+++ components/ranges/RangesRoot.js
@@ -1,7 +1,11 @@
 import { useEffect } from 'react'
+import { cloneDeep } from 'lodash'
+import Button from '@/components/common/Button'
 import Navbar from '@/components/common/Navbar'
 import Gallery from '@/components/ranges/Gallery'
 import useAuth from '@/hooks/useAuth'
+import { putUser } from '@/db/dbFetch'
+import { newRange } from '@/lib/ranges'
 import css from '@/scss/ranges/RangesRoot.module.scss'
 
 export default function RangesRoot() {
@@ -13,12 +17,30 @@ export default function RangesRoot() {
     }
   }, [isLoading])
 
+  const addSpot = () => {
+    const updatedUser = cloneDeep(user)
+
+    updatedUser.ranges.unshift(newRange)
+
+    putUser(user.email, updatedUser, () => { location.reload() })
+  }
+
   if (isLoading || !user) return null
 
   return (
     <div>
       <Navbar user={user} />
-      <div className={css.gallery}>
+      <div className={css.main}>
+        <div className={css.ui}>
+          <Button
+            large
+            theme='simple'
+            icon='plus-lg'
+            onClick={addSpot}
+          >
+            new range
+          </Button>
+        </div>
         <Gallery user={user} />
       </div>
     </div>
